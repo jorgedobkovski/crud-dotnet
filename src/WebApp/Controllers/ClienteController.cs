8a50213f87267016c7107148495fb013b480c48b
Jorge <dobkovskijorge@gmail.com>
2022-04-19 13:26:31 -0400

    Primeiro commit

---
+++ src/WebApp/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Criar()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/WebApp/Models/ClienteViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class ClienteViewModel
+    {
+        public int Id { get; set; }
+        [Required (ErrorMessage= "Erro: deu erro.")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Erro: deu erro.")]
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+    }
+}
